92be153d610ddc8fb5be3590013a8c304f3f265d
pank-su <frostkslo1@yandex.ru>
2022-12-16 13:25:57 +0300

    Initial

---
+++ WebJsonExample/Cat.cs
@@ -0,0 +1,16 @@
+﻿namespace WebJsonExample;
+
+public class Cat
+{
+    public string[] tags { get; set; }
+    public string createdAt { get; set; }
+    public string updatedAt { get; set; }
+    public bool validated { get; set; }
+    public string owner { get; set; }
+    public string file { get; set; }
+    public string mimetype { get; set; }
+    public int? size { get; set; }
+    public string?_id { get; set; }
+    public string url { get; set; }
+}
+
---
+++ WebJsonExample/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebJsonExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var response = WebRequest.Create("https://cataas.com/cat?json=true").GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string jsonText = reader.ReadToEnd();
+            JsonBox.Text = jsonText;
+            Cat? cat = Newtonsoft.Json.JsonConvert.DeserializeObject(jsonText, typeof(Cat)) as Cat;
+            JsonInfoBox.Text = @$"Тэги: {String.Join(", ", cat!.tags)}
+Cоздан: {cat!.createdAt}
+Обновлён: {cat!.updatedAt}";
+            Image.Source = new BitmapImage(new Uri("https://cataas.com"  + cat.url));
+        }
+    }
+}
